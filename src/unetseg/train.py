49bbf59b3a5b684d882e5fdcbae5687847d7bb11
Ro-devesa <Ro.devesa@dymaxionlabs.com>
2021-09-27 20:36:35 +0000

    Bugfix: mean_iou metric

--- src/unetseg/train.py
+++ src/unetseg/train.py
@@ -261,7 +261,7 @@ def build_model_unetplusplus(cfg):
         prec = []
         for t in np.arange(0.5, 1.0, 0.05):
             y_pred_ = tf.cast(y_pred > t, tf.int32)
-            score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, cfg.n_classes)
+            score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
             K.get_session().run(tf.local_variables_initializer())
             with tf.control_dependencies([up_opt]):
                 score = tf.identity(score)
