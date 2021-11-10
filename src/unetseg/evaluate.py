9433e39e8cdd9c78c3f4322774cceb3ec02f6ab9
Ro-devesa <73536713+Ro-devesa@users.noreply.github.com>
2021-11-10 09:55:43 -0300

    Update evaluate.py
    update to print less than 3 dim images

--- src/unetseg/evaluate.py
+++ src/unetseg/evaluate.py
@@ -35,7 +35,13 @@ def plot_data_generator(num_samples=3, fig_size=(20, 10), *, train_config, img_c
                     nrows=1, ncols=train_config.n_classes + 1, figsize=fig_size
                 )
 
-                image = image[:, :, :img_ch].astype(np.uint8)
+                
+                if train_config.n_channels < 4:
+                  image = image[:, :,0].astype(np.uint8)
+                else:
+                  image = image[:, :, :img_ch].astype(np.uint8)
+                
+                
                 ax[0].imshow(image)
                 for i in range(train_config.n_classes):
                     ax[i + 1].imshow(mask[:, :, i])
@@ -85,8 +91,13 @@ def plot_data_results(num_samples=3, fig_size=(20, 10), *, predict_config, img_c
                 nrows=1, ncols=predict_config.n_classes + 1, figsize=(20, 40)
             )
 
-            img_s2 = img_s2.astype(np.uint8)
-
+            
+            if train_config.n_channels < 4:
+                  img_s2 = img_s2[:, :,0].astype(np.uint8)
+            else:
+                  img_s2 = img_s2[:, :, :img_ch].astype(np.uint8)
+                
+                
             axes[0].imshow(img_s2)
             for c in range(predict_config.n_classes):
                 axes[1 + c].imshow(np.squeeze(mask_[:, :, c]))
