bf228dec9984a1b84608922126d5d54aed2a4889
Ro-devesa <73536713+Ro-devesa@users.noreply.github.com>
2021-11-16 13:57:50 -0300

    Update evaluate.py
    fixed plor data results for images of one band

--- src/unetseg/evaluate.py
+++ src/unetseg/evaluate.py
@@ -50,13 +50,9 @@ def plot_data_generator(num_samples=3, fig_size=(20, 10), *, train_config, img_c
     plot_samples(plt, data_generator, num_samples)
     plt.show()
 
-
 def plot_data_results(num_samples=3, fig_size=(20, 10), *, predict_config, img_ch=3):
 
-    if predict_config.n_channels < 4:
-        img_ch = predict_config.n_channels
-    else:
-        img_ch = img_ch
+    
 
     images = [
         os.path.basename(f)
@@ -66,18 +62,39 @@ def plot_data_results(num_samples=3, fig_size=(20, 10), *, predict_config, img_c
     images = random.sample(images, num_samples)
     for img_file in images:
         try:
-            # s2 3D
-            img_s2 = tiff.imread(
-                os.path.join(predict_config.images_path, "images", img_file)
-            )[:, :, :img_ch]
-            img_s2 = minmax_scale(img_s2.ravel(), feature_range=(0, 255)).reshape(
-                img_s2.shape
-            )
 
+            if predict_config.n_channels < 3:
+    
+                img_s2 = tiff.imread(
+                os.path.join(predict_config.images_path, "images", img_file)
+                )
+             
+
+
+                img_s2 = minmax_scale(img_s2.ravel(), feature_range=(0, 255)).reshape(
+                img_s2.shape
+                )
+                
+                
+            else:
+                img_s2 = tiff.imread(
+                os.path.join(predict_config.images_path, "images", img_file)
+                )[:, :,:img_ch ]
+
+
+
+                img_s2 = minmax_scale(img_s2.ravel(), feature_range=(0, 255)).reshape(
+                img_s2.shape
+                )
+                
+            
+              
             # Prediccion
             mask_ = (
                 tiff.imread(os.path.join(predict_config.results_path, img_file)) / 255
             )
+            mask_ = np.rollaxis(mask_, 0, 3)  
+       
             mask_ = resize(
                 mask_,
                 (predict_config.height, predict_config.width, predict_config.n_classes),
@@ -88,13 +105,9 @@ def plot_data_results(num_samples=3, fig_size=(20, 10), *, predict_config, img_c
             fig, axes = plt.subplots(
                 nrows=1, ncols=predict_config.n_classes + 1, figsize=(20, 40)
             )
+            axes[0].imshow(img_s2)  
+               
 
-            if train_config.n_channels < 4:
-                img_s2 = img_s2[:, :, 0].astype(np.uint8)
-            else:
-                img_s2 = img_s2[:, :, :img_ch].astype(np.uint8)
-
-            axes[0].imshow(img_s2)
             for c in range(predict_config.n_classes):
                 axes[1 + c].imshow(np.squeeze(mask_[:, :, c]))
 
