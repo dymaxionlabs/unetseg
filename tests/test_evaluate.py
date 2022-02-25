08c79e8533fe1a7476878f57924c59d30be91d75
Damián Silvani <munshkr@gmail.com>
2022-02-25 15:18:28 -0300

    Create tests placeholders

---
+++ tests/test_evaluate.py
@@ -0,0 +1,11 @@
+import pytest
+
+from unetseg.evaluate import plot_data_generator, plot_data_results
+
+__author__ = "Damián Silvani"
+__copyright__ = "Dymaxion Labs"
+__license__ = "apache-2.0"
+
+
+def test_main():
+    assert True
---
+++ tests/test_train.py
@@ -0,0 +1,11 @@
+import pytest
+
+from unetseg.train import TrainConfig, train
+
+__author__ = "Damián Silvani"
+__copyright__ = "Dymaxion Labs"
+__license__ = "apache-2.0"
+
+
+def test_main():
+    assert True
