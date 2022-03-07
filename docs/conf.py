c3b5000286efea4f1eaad727df56739c51ba6964
Damián Silvani <munshkr@gmail.com>
2022-03-07 13:12:07 -0300

    Fix path to src/

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ __location__ = os.path.join(
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.join(__location__, "../src"))
+sys.path.insert(0, os.path.join(__location__, ".."))
 
 # -- Run sphinx-apidoc -------------------------------------------------------
 # This hack is necessary since RTD does not issue `sphinx-apidoc` before running
@@ -38,7 +38,7 @@ except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../src/unetseg")
+module_dir = os.path.join(__location__, "../unetseg")
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
